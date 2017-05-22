84ec3b29135d8236275b9b68667371010ef55dec
Felix Gonschorek <felix@netzgut.net>
2017-05-22 20:44:47 +0200

    improve pin layout: move rain sensor from PIN 11 to PIN 18

--- src/main/java/de/whw/anemo/WHWConstants.java
+++ src/main/java/de/whw/anemo/WHWConstants.java
@@ -6,11 +6,14 @@ import com.pi4j.io.gpio.RaspiPin;
 
 public interface WHWConstants {
 
+    // GPIO Pins are numbered by WiringPI numbers, see:
+    // https://pinout.xyz/pinout/pin8_gpio14
     public static final Pin OUTPUT_LED_PIN        = RaspiPin.GPIO_01;
 
     public static final Pin SENSOR_ANEMOMETER_PIN = RaspiPin.GPIO_04;
-    public static final Pin SENSOR_RAIN_PIN       = RaspiPin.GPIO_00;
+    public static final Pin SENSOR_RAIN_PIN       = RaspiPin.GPIO_05;
 
+    // These are the pins on the ADS1115 analog to digital converter
     public static final Pin SENSOR_ADS1115_A0     = ADS1115Pin.INPUT_A0;
     public static final Pin SENSOR_ADS1115_A1     = ADS1115Pin.INPUT_A1;
 
